93684b43a29227629e59c2b75452aa86befc4b15
eb_cmgonzalezo <eb_cmgonzalezo@bancoripley.com>
2023-02-04 21:36:44 -0300

    diseno del login, funcionamiento login,  conf socket.io

--- models/server.js
+++ models/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const { createServer } = require('http')
 
 const { dbConnection } = require('../database/config');
+const { socketController } = require('../socket/socketControllers');
 
 class Server {
 
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
+
+        this.server = createServer( this.app )
+        this.io     = require('socket.io')( this.server )
 
         this.paths = {
             auth:       '/api/auth',
@@ -28,6 +33,9 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes();
+
+        // Sockets
+        this.sockets();
     }
 
     async conectarDB() {
@@ -56,18 +64,22 @@ class Server {
     }
 
     routes() {
-        
+
         this.app.use( this.paths.auth, require('../routes/auth'));
         this.app.use( this.paths.buscar, require('../routes/buscar'));
         this.app.use( this.paths.categorias, require('../routes/categorias'));
         this.app.use( this.paths.productos, require('../routes/productos'));
         this.app.use( this.paths.usuarios, require('../routes/usuarios'));
         this.app.use( this.paths.uploads, require('../routes/uploads'));
-        
+
+    }
+
+    sockets(){
+        this.io.on( 'connection', socketController )
     }
 
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
---
+++ public/js/auth.js
@@ -0,0 +1,76 @@
+let url = (window.location.hostname.includes('localhost') )
+? 'http://localhost:8080/api/auth/'
+: 'https://abcde.herokuapp.com/api/auth/'
+
+
+const formulario = document.querySelector('form')
+formulario.addEventListener( 'submit', e => {
+    e.preventDefault();
+    const formData = {}
+
+    for (const ele of formulario.elements) {
+        if (ele.name.length > 0) {
+            formData[ele.name] = ele.value
+        }
+    }
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    fetch( url + 'login', { method: 'POST',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default',
+        body: JSON.stringify(formData)
+    })
+        .then( resp => resp.json() )
+        .then( ({ msg, token }) => {
+            if (msg) {
+                return console.error( msg )
+            }
+
+            localStorage.setItem('token', token)
+        })
+        .catch( err => {
+            console.log(err);
+        })
+})
+
+// Google Token - ID TOKEN
+function handleCredentialResponse(response) {
+    const id_token = response.credential
+    var body = { id_token };
+
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var myInit = { method: 'POST',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default',
+        body: JSON.stringify(body) };
+
+    var myRequest = new Request(url + 'google', myInit);
+
+    fetch( myRequest )
+        .then(resp => resp.json() )
+        .then( ({token}) => {
+            // console.log(data)
+            localStorage.setItem( 'token', token )
+            console.log('❤️');
+        })
+        .catch( console.log );
+}
+
+const button = document.getElementById('google-signout');
+button.onclick = () => {
+    console.log(google.accounts.id);
+    google.accounts.id.disableAutoSelect()
+
+
+    google.accounts.id.revoke( localStorage.getItem('token'), done => {
+        localStorage.clear()
+        location.reload()
+    } )
+}
